3f2537b9d95b572843f29e5287f97ab61cd6a726
henrygo0985 <151686750+henrygo0985@users.noreply.github.com>
2024-04-06 12:03:36 -0600

    Update ProjectConfig.java

--- src/main/java/com/proyectoElSaber/ProjectConfig.java
+++ src/main/java/com/proyectoElSaber/ProjectConfig.java
@@ -4,23 +4,102 @@
  */
 package com.proyectoElSaber;
 
+import java.util.Locale;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
-/**
- *
- * @author Personal
- */
-public class ProjectConfig {
+@Configuration
+public class ProjectConfig implements WebMvcConfigurer {
 
-    //Bean para poder acceder a los Messages.properties en código...
+    /* Los siguientes métodos son para incorporar el tema de internacionalización en el proyecto */
+
+ /* localeResolver se utiliza para crear una sesión de cambio de idioma */
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    /* Métodos para internacionalización */
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        var slr = new SessionLocaleResolver();
+        slr.setDefaultLocale(Locale.getDefault());
+        slr.setLocaleAttributeName("session.current.locale");
+        slr.setTimeZoneAttributeName("session.current.timezone");
+        return slr;
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        var lci = new LocaleChangeInterceptor();
+        lci.setParamName("lang");
+        return lci;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+
     @Bean("messageSource")
     public MessageSource messageSource() {
         ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
         messageSource.setBasenames("messages");
         messageSource.setDefaultEncoding("UTF-8");
         return messageSource;
+    }
 
+    /* Métodos de seguridad */
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("index");
+        registry.addViewController("/index").setViewName("index");
+        registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/admisiones").setViewName("admisiones");
+        registry.addViewController("/contacto").setViewName("contacto");
+        registry.addViewController("/registroEstudiantes").setViewName("registroEstudiantes");
+        registry.addViewController("/notas").setViewName("notas");
+    }
+    
+    
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((request) -> request
+                        .requestMatchers("/", "/index", "/errores/**",
+                                "/js/**", "/webjars/**")
+                        .permitAll()
+                        .requestMatchers("/registroEstudiantes")
+                        .hasRole("ADMIN")
+                        .requestMatchers("/notas")
+                        .hasRole("ESTUDIANTE")
+                        .anyRequest().authenticated()
+                )
+                .formLogin((form) -> form
+                        .loginPage("/login").permitAll())
+                .logout((logout) -> logout.permitAll());
+        return http.build();
+    }
+
+    @Autowired
+    public void configurerGlobal(AuthenticationManagerBuilder build) throws Exception {
+        build.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
     }
 }
+
